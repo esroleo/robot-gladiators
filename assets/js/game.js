d5ad2b7bb58c0e03f0bfd885fce1cae8b50fa8c0
Esteban Romero <esroleo@gmail.com>
2020-12-14 11:18:21 -0500

    js test on functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,33 @@
-window.alert("This is an alert! JavaScript is running!");
+// Players name function
+var playerName = window.prompt("What is your robot's name?");
+
+// Display the name back to the user
+//window.alert(playerName);
+// Display the name of at the console 
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+// This creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// Lets call our fight fucntion
+// fight();
+
