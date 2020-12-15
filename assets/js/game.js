3b0f5ec45c44a1196da5adcacb47089e7b5704d0
Esteban Romero <esroleo@gmail.com>
2020-12-15 11:44:51 -0500

    loops updated for better flow of game

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,8 +20,8 @@ console.log("Your robot, " + playerName + ", has won!");
 */
 
 //Welcome the player 
-
-window.alert("Welcome to Robot Gladiators!");
+// Moved to the for loop of our game counter
+//window.alert("Welcome to Robot Gladiators!");
 
 // Players information
 
@@ -120,9 +120,23 @@ var fight = function(enemyName) {
 
 // Call the fight function and pass enemyName argument 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 20;
-    fight(pickedEnemyName);
+    //Welcome the player 
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+      }
   }
 
 
